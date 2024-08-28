f6f506dc053287d33c6659dd8be0e71f58acf20d
ourique787 <lucastrajano93@gmail.com>
2024-08-27 21:35:50 -0300

    calculadora

---
+++ Calculadora.cs
@@ -0,0 +1,15 @@
+class Calculadora{
+    public double Soma(double x, double y){
+        double resultado = x + y;
+        return resultado;
+    }
+    public double Subtracao(double x, double y){
+        return x - y;
+    }
+    public double Multiplicacao(double x, double y){
+        return x * y;
+    }
+    public double Divisao(double x, double y){
+        return x / y;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Calculadora calc = new Calculadora();
+Console.WriteLine("Digite o valor de x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite o valor de y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+double resultadoSoma = calc.Soma(x,y);
+Console.WriteLine($"O resultado da soma de {x} e {y} e igual a: {resultadoSoma}");
+
+Console.WriteLine($"Resultado da subtracao e: {calc.Subtracao(x,y)}");
+
+Console.WriteLine($"O resultado da multiplicacao e: {calc.Multiplicacao(x,y)}");
+
+Console.WriteLine($"O resultado da divisao e: {calc.Divisao(x,y)}");
+
+
+
